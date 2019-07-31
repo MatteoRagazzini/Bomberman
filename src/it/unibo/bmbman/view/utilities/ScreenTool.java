ffdc4a38e1cd5036c3aa0beaeaefd3178fde69d5
Paolo <paolo.penazzi@studio.unibo.it>
2019-07-31 11:09:20 +0200

    Add methods to ScreenTool class

--- src/it/unibo/bmbman/view/utilities/ScreenTool.java
+++ src/it/unibo/bmbman/view/utilities/ScreenTool.java
@@ -2,7 +2,7 @@ package it.unibo.bmbman.view.utilities;
 
 import java.awt.Toolkit;
 /**
- * .
+ * Screen resolution utility class.
  */
 public class ScreenTool {
     private static final double SCREEN_WIDTH = Toolkit.getDefaultToolkit().getScreenSize().getWidth();
@@ -12,11 +12,22 @@ public class ScreenTool {
     private static final Float NEW_SIZE = 50f;
     private final GameFont font = new GameFont();
     /**
-     * 
+     * Method to adjust font size based on screen resolution.
      */
     public void checkFontSize() {
-        if (SCREEN_WIDTH > WIDTH_REF && SCREEN_HEIGHT > HEIGHT_REF) {
+        if (getScreenRes().equals("4k")) {
             font.setFontSize(NEW_SIZE);
+        }
+    }
+    /**
+     * Method to get the screen resolution.
+     * @return the screen resolution
+     */
+    public String getScreenRes() {
+        if (SCREEN_WIDTH > WIDTH_REF && SCREEN_HEIGHT > HEIGHT_REF) {
+            return "4k";
+        } else {
+            return "HD";
         }
     }
 }
