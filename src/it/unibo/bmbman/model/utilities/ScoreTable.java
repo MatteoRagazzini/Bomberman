b0bf93ce924c1c35579fb379eab3e8bf95052a0c
Lucia <lucia.sternini2@studio.unibo.it>
2019-08-03 13:42:08 +0200

    Change ScoreTable's constructor

--- src/it/unibo/bmbman/model/utilities/ScoreTable.java
+++ src/it/unibo/bmbman/model/utilities/ScoreTable.java
@@ -1,4 +1,5 @@
 package it.unibo.bmbman.model.utilities;
+import java.util.List;
 
 import javax.swing.table.AbstractTableModel;
 /**
@@ -9,7 +10,25 @@ import javax.swing.table.AbstractTableModel;
 public class ScoreTable extends AbstractTableModel {
     private static final long serialVersionUID = 1L;
     private String[] column = {"Rank", "Name", "Score", "Time"};
-    private Object[][] data = {{1, "Lucia", 100, "01:22"}, {null, null, null, null}};
+    private String[][] data;
+    /**
+     * 
+     * @param list of data to populate the table.
+     */
+    public ScoreTable(final List<List<String>> list) {
+        super();
+        int rowIndex = 0;
+        int colIndex;
+        data = new String[list.size()][this.getColumnCount()];
+        for (List<String> subList : list) {
+            colIndex = 1;
+            for (String string : subList) {
+                data[rowIndex][colIndex] = string;
+                colIndex++;
+            }
+            rowIndex++;
+        }
+    }
     /**
      * {@inheritDoc}
      */
