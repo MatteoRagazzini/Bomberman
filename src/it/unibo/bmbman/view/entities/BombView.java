4603a003d2a1511f6647301d8b8fac6771fcdc07
Matteo Ragazzini <matteo.ragazzini5@studio.unibo.it>
2019-08-09 13:17:53 +0200

    add class bomb View and its spritesheet

---
+++ src/it/unibo/bmbman/view/entities/BombView.java
@@ -0,0 +1,40 @@
+package it.unibo.bmbman.view.entities;
+
+
+import java.awt.Image;
+import java.util.EnumMap;
+import java.util.Map;
+import it.unibo.bmbman.model.Direction;
+import it.unibo.bmbman.model.utilities.Dimension;
+import it.unibo.bmbman.model.utilities.Position;
+import it.unibo.bmbman.view.utilities.Animation;
+import it.unibo.bmbman.view.utilities.AnimationImpl;
+/**
+ * Class to manage the view of monsters.
+ *
+ *
+ */
+public class BombView extends AbstractEntityView {
+    private static final String PATH_BOMB_IMAGES = "/bombs/bomb";
+    private static final int DIMENSION = 40;
+    private static final int FRAME_PER_ANIMATION = 2;
+    private final Animation sprites = new AnimationImpl();
+
+    /**
+     * Create a monster view.
+     * @param position where the monster is located
+     * @param dimension the dimension of the monster
+     */
+    public BombView(final Position position, final Dimension dimension) {
+        super(position, new Dimension(DIMENSION, DIMENSION), true); 
+        this.sprites.createAnimation(PATH_BOMB_IMAGES, FRAME_PER_ANIMATION, DIMENSION);
+    }
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Image getSprite() {
+        return this.sprites.getNextImage();
+    }
+}
+
