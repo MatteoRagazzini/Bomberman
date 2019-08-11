4816ea4677428977b71796c1be68ba673fd41ff8
Marta Spadoni <marta.spadoni2@studio.unibo.it>
2019-08-11 12:34:43 +0200

    Add in Model EntityCollisionManager, the new version of CollisionController

---
+++ src/it/unibo/bmbman/model/EntityCollisionManager.java
@@ -0,0 +1,23 @@
+package it.unibo.bmbman.model;
+
+import java.util.Set;
+
+import it.unibo.bmbman.model.utilities.Position;
+
+/**
+ * Collision Manager.
+ */
+public interface EntityCollisionManager {
+    /**
+     * Determines if the followed entity is in collision with one of the entity in set.
+     * @param entities set of {@link Entity}
+     */
+    void collision(Set<Entity> entities);
+    /**
+     * Used to notify the followed entity a collision.
+     * @param receiver {@link Entity} with which the collision occurred
+     * @param newPosition new entity's position to avoid continuous collisions
+     */
+    void notifyCollision(Entity receiver, Position newPosition);
+
+}
