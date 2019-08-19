992727b8db6f5c6cebb3d8211fbff3c2b7ebd6bc
Marta Spadoni <marta.spadoni2@studio.unibo.it>
2019-08-19 17:48:46 +0200

    fix testCollision position

--- src/it/unibo/bmbman/test/TestCollision.java
+++ src/it/unibo/bmbman/test/TestCollision.java
@@ -19,6 +19,7 @@ import it.unibo.bmbman.model.entities.Monster;
 import it.unibo.bmbman.model.entities.Wall;
 import it.unibo.bmbman.model.utilities.Dimension;
 import it.unibo.bmbman.model.utilities.Position;
+import it.unibo.bmbman.view.utilities.ScreenToolUtils;
 
 /**
  * JUnit Test for  Collision Manager and entityController.
@@ -26,10 +27,10 @@ import it.unibo.bmbman.model.utilities.Position;
  */
 public class TestCollision {
     private static final int HERO_LIVES = 3;
-    private static final Position MONSTER_POSITION = new Position(60, 70);
-    private static final Position WALL_POSITION = new Position(100, 50);
-    private static final Position NEAR_WALL_POSITION = new Position(WALL_POSITION.getX() - TerrainFactoryImpl.CELL_DIMENSION / 2,
-                                                                    WALL_POSITION.getY() - TerrainFactoryImpl.CELL_DIMENSION / 2);
+    private static final Position MONSTER_POSITION = new Position(60 * ScreenToolUtils.SCALE, 70 * ScreenToolUtils.SCALE);
+    private static final Position WALL_POSITION = new Position(100 * ScreenToolUtils.SCALE, 50 * ScreenToolUtils.SCALE);
+    private static final Position NEAR_WALL_POSITION = new Position(WALL_POSITION.getX() - TerrainFactoryImpl.CELL_DIMENSION * ScreenToolUtils.SCALE / 2,
+                                                                    WALL_POSITION.getY() - TerrainFactoryImpl.CELL_DIMENSION * ScreenToolUtils.SCALE / 2);
     private final Hero hero = new HeroImpl();
     private final Monster monster = new Monster(MONSTER_POSITION);
     private final Wall wall = new Wall(WALL_POSITION, new Dimension(TerrainFactoryImpl.CELL_DIMENSION, TerrainFactoryImpl.CELL_DIMENSION));
